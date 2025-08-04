904f7462b88aa23eb0fa0d2372cdc7345eee8319
Naser Ahadi <n.ahadi@vandar.io>
2025-08-04 10:02:19 +0330

    create Auth Context

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,57 @@
+import { useReducer, createContext, useContext } from "react";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error(`Unknown action type ${action.type}`);
+  }
+}
+
+const AuthContext = createContext(null);
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  function login(email, password) {
+    if (FAKE_USER.email === email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a Provider");
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
